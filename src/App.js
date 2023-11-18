5ae6d043bc8245ba4a64fe0f09c7347238df2894
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-11-18 14:39:19 -0500

    initial rainbow created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
+import React, {useState } from 'react'
+import ColorBlock from './Components/ColorBlock'
 
 function App() {
+  let colors = ['violet','blue', 'lightblue','green','greenyellow','yellow','orange','red']
+  let colorMap = colors.map ((color,i) => {
+    return (
+      <ColorBlock color={color}/>
+    )
+  })
+
   return (
-    <div>
-    
+    <div className="App">
+      {colorMap}
     </div>
   );
 }
 
 export default App;
+
+// component renders a given color
+// component hold a form allows a user to enter a color
+// State value that will be tracked and changed
